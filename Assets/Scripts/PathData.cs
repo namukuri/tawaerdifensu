ac878c5e84e5687884d6b67029c1bd28212035cc
namukuri <namukuri@gmail.com>
2025-01-17 20:12:21 +0900

    手順７まで

---
+++ Assets/Scripts/PathData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathData : MonoBehaviour
+{
+    public Transform generateTran; // “G‚Ì¶¬’n“_
+    public Transform[] pathTranArray; // “G‚ÌˆÚ“®Œo˜H‚ğ‡”Ô‚É“o˜^‚·‚é
+
+}
