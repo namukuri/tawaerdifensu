0b5b93c06b011d3c3558090428b19f208fd9aba6
namukuri <namukuri@gmail.com>
2025-01-19 14:28:55 +0900

    手順８まで

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq; //<=　宣言を追加します。
+
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField, Header("移動経路の情報")]
+    private PathData pathData;
+
+    [SerializeField, Header("移動速度")]
+    private float moveSpeed;
+
+    private Vector3[] paths; // 移動する各地点を代入するための配列
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // 移動する地点を取得
+        paths = pathData.pathTranArray.Select(x => x.position).ToArray();
+
+        // 移動する地点を取得するための配列の初期化
+        // paths = new Vector3[pathData.pathTranArray.Length];
+
+        // 移動する位置情報を順番に配列に取得
+        //for (int i = 0; i < pathData.pathTranArray.Length; i++)
+        //{
+        //  paths[i] = pathData.pathTranArray[i].position;
+        //}
+
+        // 各地点に向けて移動
+        transform.DOPath(paths, 1000 /  moveSpeed).SetEase(Ease.Linear);
+    }      
+    
+}
